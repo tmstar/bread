bc2f9bec1c3873770d314ec46490ac9b942c35c7
Tomoaki Watanabe <hswjg518@gmail.com>
2021-01-16 23:45:47 +0900

    update and delete button function

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,15 @@
 import React from "react";
 
-function TodoList({ todos }) {
-  const todoList = todos.map(todo => {
+function TodoList({ todos, toggleTodo, deleteTodo }) {
+  const todoList = todos.map((todo) => {
     const label = todo.completed ? "作業中にする" : "完了にする";
     return (
       <li key={todo.id}>
         {todo.title}
-        <button>{label}</button>
-        <button>削除</button>
+        <button onClick={() => toggleTodo(todo.id, todo.completed)}>
+          {label}
+        </button>
+        <button onClick={() => deleteTodo(todo.id)}>削除</button>
       </li>
     );
   });
