bed505ccc8b807305678e4abb501d14853a75a80
Tomoaki Watanabe <hswjg518@gmail.com>
2021-01-16 23:45:09 +0900

    add, delete, update todos

--- src/hooks/useTodo.js
+++ src/hooks/useTodo.js
@@ -1,12 +1,40 @@
 import { useState, useEffect } from "react";
+import { uuid } from "uuidv4";
 import todoService from "../services/todos";
 
 export default function useTodo() {
   const [todos, setTodos] = useState([]);
   useEffect(() => {
-    todoService.getAll().then(todos => {
+    todoService.getAll().then((todos) => {
       setTodos(todos.reverse());
     });
   }, []);
-  return { todos };
+
+  const toggleTodo = (id, completed) => {
+    const todo = todos.find((todo) => todo.id === id);
+    const newTodo = { ...todo, completed: !completed };
+
+    todoService.update(id, newTodo).then((updatedTodo) => {
+      const newTodos = todos.map((todo) =>
+        todo.id !== updatedTodo.id ? todo : updatedTodo
+      );
+      setTodos(newTodos);
+    });
+  };
+
+  const deleteTodo = (id) => {
+    todoService.delete(id).then((deletedTodoId) => {
+      const newTodos = todos.filter((todo) => todo.id !== deletedTodoId);
+      setTodos(newTodos);
+    });
+  };
+
+  const addTodo = (todo) => {
+    const newTodo = { title: todo, completed: false, id: uuid() };
+    return todoService.add(newTodo).then((addedTodo) => {
+      setTodos([addedTodo].concat(todos));
+    });
+  };
+
+  return { todos, toggleTodo, deleteTodo, addTodo };
 }
--- src/services/todos.js
+++ src/services/todos.js
@@ -1,10 +1,25 @@
 import axios from "axios";
 
-const baseUrl = 'http://localhost:3001/todos';
+const baseUrl = "http://localhost:3001/todos";
 
 const getAll = async () => {
   const response = await axios.get(baseUrl);
   return response.data;
 };
 
-export default { getAll };
+const update = async (id, newTodo) => {
+  const response = await axios.put(`${baseUrl}/${id}`, newTodo);
+  return response.data;
+};
+
+const _delete = async (id) => {
+  await axios.delete(`${baseUrl}/${id}`);
+  return id;
+};
+
+const add = async (newTodo) => {
+  const response = await axios.post(`${baseUrl}`, newTodo);
+  return response.data;
+};
+
+export default { getAll, update, delete: _delete, add };
