ff9720d4601bc6a7622ce24049d99734dda8dac1
Tomoaki Watanabe <hswjg518@gmail.com>
2021-01-17 00:30:40 +0900

    add filter elements function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import TodoList from "./components/TodoList";
 import TodoForm from "./components/TodoForm";
 import TodoFilter from "./components/TodoFilter";
@@ -8,15 +8,30 @@ import useTodo from "./hooks/useTodo";
 function App() {
   const { todos, toggleTodo, deleteTodo, addTodo } = useTodo();
   const [filter, setFilter] = useState("all");
-  const handleFilter = event =>{
+  const handleFilter = (event) => {
     setFilter(event.target.value);
-  }
+  };
+  const filteredTodos = useMemo(() => {
+    switch (filter) {
+      case "inProgress":
+        return todos.filter((todo) => !todo.completed);
+      case "completed":
+        return todos.filter((todo) => todo.completed);
+      case "all":
+      default:
+        return todos;
+    }
+  }, [todos, filter]);
   return (
     <div className="App">
       <h1>TodoList</h1>
       <TodoFilter handleFilter={handleFilter} selectedFilter={filter} />
       <TodoForm addTodo={addTodo} />
-      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+      <TodoList
+        todos={filteredTodos}
+        toggleTodo={toggleTodo}
+        deleteTodo={deleteTodo}
+      />
     </div>
   );
 }
