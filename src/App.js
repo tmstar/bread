1ab3c5ee2e0b1fd8a855699222908c612ef48cbb
Tomoaki Watanabe <hswjg518@gmail.com>
2021-01-17 00:10:48 +0900

    add filter component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import "./App.css";
+import React, { useState } from "react";
 import TodoList from "./components/TodoList";
 import TodoForm from "./components/TodoForm";
+import TodoFilter from "./components/TodoFilter";
 import useTodo from "./hooks/useTodo";
 
 function App() {
   const { todos, toggleTodo, deleteTodo, addTodo } = useTodo();
+  const [filter, setFilter] = useState("all");
+  const handleFilter = event =>{
+    setFilter(event.target.value);
+  }
   return (
     <div className="App">
       <h1>TodoList</h1>
+      <TodoFilter handleFilter={handleFilter} selectedFilter={filter} />
       <TodoForm addTodo={addTodo} />
-      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
     </div>
   );
 }
---
+++ src/components/TodoFilter.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const filters = [
+  { type: "all", label: "すべて" },
+  { type: "inProgress", label: "作業中" },
+  { type: "completed", label: "完了" },
+];
+
+function TodoFilter({selectedFilter, handleFilter}) {
+  return filters.map((filter) => (
+    <label key={filter.type}>
+      <input type="radio" value={filter.type} checked={filter.type === selectedFilter} onChange={handleFilter} />
+      {filter.label}
+    </label>
+  ));
+}
+
+export default TodoFilter;
