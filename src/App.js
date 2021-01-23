6c06294a8fb942f391270f181366eca99f54d979
Tomoaki Watanabe <hswjg518@gmail.com>
2021-01-23 12:22:29 +0900

    update font family

--- src/App.js
+++ src/App.js
@@ -7,24 +7,61 @@ import Home from "./components/Home";
 import Login from "./components/Login";
 import Logout from "./components/Logout";
 import { AuthProvider } from "./services/authProvider";
+import { MuiThemeProvider, unstable_createMuiStrictModeTheme as createMuiTheme } from "@material-ui/core/styles";
+import { blue, grey } from "@material-ui/core/colors";
+
+const darkTheme = createMuiTheme({
+  palette: {
+    type: "dark",
+    primary: {
+      light: "#eeffff",
+      main: blue["100"],
+      dark: "#8aacc8",
+    },
+    secondary: {
+      light: "#ffffff",
+      main: grey["300"],
+      dark: "#aeaeae",
+    },
+  },
+  props: {
+    MuiCheckbox: {
+      color: "primary",
+    },
+    MuiRadio: {
+      color: "primary",
+    },
+    MuiSwitch: {
+      color: "primary",
+    },
+    MuiList: {
+      dense: true,
+    },
+  },
+  typography: {
+    fontFamily: `"Prompt","Yu Gothic Medium","游ゴシック Medium",YuGothic,"游ゴシック体","Roboto","Helvetica","Arial",sans-serif`,
+  },
+});
 
 function App() {
   return (
-    <div className="App">
+    <MuiThemeProvider theme={darkTheme}>
       <CssBaseline />
-      <header>
-        <AuthProvider>
-          <BrowserRouter>
-            <Switch>
-              <Redirect exact path="/" to="/login" />
-              <Route path="/login" component={Login} />
-              <Route path="/logout" component={Logout} />
-              <AuthRoute exact path="/home" component={Home} />
-            </Switch>
-          </BrowserRouter>
-        </AuthProvider>
-      </header>
-    </div>
+      <div className="App">
+        <header>
+          <AuthProvider>
+            <BrowserRouter>
+              <Switch>
+                <Redirect exact path="/" to="/login" />
+                <Route path="/login" component={Login} />
+                <Route path="/logout" component={Logout} />
+                <AuthRoute exact path="/home" component={Home} />
+              </Switch>
+            </BrowserRouter>
+          </AuthProvider>
+        </header>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,39 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { MuiThemeProvider, unstable_createMuiStrictModeTheme as createMuiTheme } from "@material-ui/core/styles";
-import { blue } from "@material-ui/core/colors";
-
-const darkTheme = createMuiTheme({
-  palette: {
-    type: "dark",
-    primary: {
-      light: blue["300"],
-      main: blue["500"],
-      dark: blue["700"],
-    },
-  },
-  props: {
-    MuiCheckbox: {
-      color: "primary",
-    },
-    MuiRadio: {
-      color: "primary",
-    },
-    MuiSwitch: {
-      color: "primary",
-    },
-    MuiList: {
-      dense: true,
-    },
-  },
-});
 
 ReactDOM.render(
   <React.StrictMode>
-    <MuiThemeProvider theme={darkTheme}>
-      <App />
-    </MuiThemeProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
