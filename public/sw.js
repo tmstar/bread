89eb34414f0550c602b413824c461cfee803d298
Tomoaki Watanabe <hswjg518@gmail.com>
2021-02-07 16:43:54 +0900

    update service worker with cache

---
+++ public/sw.js
@@ -0,0 +1,43 @@
+const cacheName = "bread-v0.1";
+const urlsToCache = [
+  "/static/js/bundle.js",
+  "/static/js/0.chunk.js",
+  "/static/js/main.chunk.js",
+  "/favicon.ico",
+  "/home",
+  "/manifest.json",
+  "/logo192.png",
+];
+
+self.addEventListener("install", (event) => {
+  console.log("auto installing...");
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      console.log("cache creating...");
+      cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        console.log("old cache deleting...");
+        return Promise.all(cacheNames.filter((name) => name !== cacheName).map((name) => caches.delete(name)));
+      })
+      .then(() => {
+        clients.claim();
+      })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
