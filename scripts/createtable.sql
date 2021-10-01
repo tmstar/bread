5fe89579e1737d3064e2a7341c205174e5b01709
tmstar <hswjg518@gmail.com>
2021-10-01 15:23:20 +0900

    Update tag view after mod (#3)
    * create user tag view for access control
    
    * no-wrap too-long tag name
    
    * clear tag edit box after enter
    
    * update tag list pane after mod tags in list

--- scripts/createtable.sql
+++ scripts/createtable.sql
@@ -2,8 +2,9 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE users (
   id text PRIMARY KEY,
   name text NOT NULL,
-  email text NOT NULL UNIQUE,
-  created_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP
+  email text NOT NULL,
+  created_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  last_seen timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 CREATE TABLE item (
   id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
@@ -24,6 +25,7 @@ CREATE TABLE tag (
   id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
   name text NOT NULL,
   user_id text REFERENCES users(id),
+  shared_to text REFERENCES users(id),
   UNIQUE (name, user_id)
 );
 CREATE TABLE item_list_tag (
@@ -31,3 +33,11 @@ CREATE TABLE item_list_tag (
   tag_id uuid REFERENCES tag(id),
   PRIMARY KEY (item_list_id, tag_id)
 );
+CREATE OR REPLACE VIEW user_tag AS
+SELECT t.id AS tag_id,
+  u.id AS user_id,
+  l.id AS item_list_id
+FROM tag t,
+  users u,
+  item_list l
+WHERE (t.name = u.email);
