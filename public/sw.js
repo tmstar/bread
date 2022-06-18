210b01252d2153c55b4587d0415b016e6ec3c924
Tomoaki Watanabe <hswjg518@gmail.com>
2022-06-18 20:35:23 +0900

    delete old sw cache

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,10 @@
-const cacheName = 'bread-v0.5.5';
+const cacheName = 'bread-v0.5.6';
 const urlsToCache = [
-  '/',
-  '/static/media/eating_together.964f55d4c88af0548e5eed64357bed2d.svg',
-  '/favicon.ico',
-  '/manifest.json',
-  '/logo192.png',
+  // '/',
+  // '/static/media/eating_together.964f55d4c88af0548e5eed64357bed2d.svg',
+  // '/favicon.ico',
+  // '/manifest.json',
+  // '/logo192.png',
 ];
 
 self.addEventListener('install', (event) => {
