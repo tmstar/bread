5498cfa6f337285607843bf318327915145def50
Tomoaki Watanabe <hswjg518@gmail.com>
2022-06-21 00:02:14 +0900

    remove redirect

--- src/components/routing/ProtectedRoute.js
+++ src/components/routing/ProtectedRoute.js
@@ -1,14 +1,8 @@
-import { useAuth0, withAuthenticationRequired } from '@auth0/auth0-react';
+import { withAuthenticationRequired } from '@auth0/auth0-react';
 import React from 'react';
-import { Navigate } from 'react-router-dom';
 import Loader from './Loader';
 
 function ProtectedRoute({ component, ...options }) {
-  const { isLoading, isAuthenticated } = useAuth0();
-
-  if (isLoading) return <Loader />;
-  if (!isAuthenticated) return <Navigate to="/login" replace />;
-
   const Component = withAuthenticationRequired(component, {
     onRedirecting: () => <Loader />,
   });
