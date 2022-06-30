2756a6b53b841d4b6d6158e526cfb9dc589c7dc8
Tomoaki Watanabe <hswjg518@gmail.com>
2022-06-30 10:28:32 +0900

    add demo data for first login

---
+++ src/hooks/DemoDataHooks.js
@@ -0,0 +1,78 @@
+import { useAuth0 } from '@auth0/auth0-react';
+import gql from 'graphql-tag';
+import { useMutation } from 'react-apollo';
+import { v4 as uuid_v4 } from 'uuid';
+import getUuid from 'uuid-by-string';
+import { demoText } from './DemoData';
+
+const CREATE_DEMO_LISTS = gql`
+  mutation CreateDemoLists($list: [item_list_insert_input!]!) {
+    insert_item_list(objects: $list, on_conflict: { constraint: item_list_pkey, update_columns: id }) {
+      affected_rows
+    }
+  }
+`;
+
+const demoData = (userId) => {
+  const txt = demoText;
+  return txt.lists.reverse().map((list, index) => {
+    const listId = getUuid(`${userId}-demo-${index}`);
+    return {
+      id: listId,
+      user_id: userId,
+      name: list.name,
+      item_list_tags: {
+        data: list.tags.map((tag) => {
+          const tagId = uuid_v4();
+          return {
+            tag: {
+              data: {
+                id: tagId,
+                user_id: userId,
+                name: tag.name,
+              },
+              on_conflict: { constraint: 'tag_name_user_id_key', update_columns: 'name' },
+            },
+          };
+        }),
+        on_conflict: { constraint: 'item_list_tag_pkey', update_columns: 'tag_id' },
+      },
+      items: {
+        data: list.items.map((item, index) => {
+          return {
+            id: getUuid(`${listId}-${index}`),
+            title: item.title,
+            note: item.note,
+            color: item.color ?? 'default',
+            completed: item.completed ?? false,
+            is_active: true,
+            position: 100 - index,
+          };
+        }),
+        on_conflict: { constraint: 'wish_list_pkey', update_columns: [] },
+      },
+    };
+  });
+};
+
+export const useCreateDemoItems = () => {
+  const { user } = useAuth0();
+  const [createList, { error }] = useMutation(CREATE_DEMO_LISTS);
+
+  const createDemoItems = () => {
+    const userMetadata = user[process.env.REACT_APP_AUTH0_TOKEN_KEY + '/user_metadata'];
+    if (!(userMetadata?.logins === 1 && userMetadata?.firstLogin)) {
+      return;
+    }
+
+    const lists = demoData(user.sub);
+
+    // controlling the mutation order
+    return createList({ variables: { list: lists[0] } })
+      .then(() => createList({ variables: { list: lists[1] } }))
+      .then(() => createList({ variables: { list: lists[2] } }));
+  };
+
+  error && console.warn(error);
+  return { createDemoItems };
+};
